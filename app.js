9e7564e7fbbbadb37d4899c2df344c331b6f4347
OLIVIA LAM <olivialam@MacBook-de-OLIVIA.local>
2018-03-06 09:52:44 -0600

    soemthing

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+/**
+ * Modules
+ */
+const nunjucks = require('nunjucks');
 const express = require('express');
 const app = express();
 
 /**
  * Welcome message for requesting root URI
  */
-app.get('/', (req, res) => {
-    res.send('Welcome'); 
+app.get('/', (req, res, next) => {
+    res.send('Welcome');
+    next();
 });
 
-app.get('/news', (req, res) => {
+app.get('/news', (req, res, next) => {
     res.send('NEWS');
+    next();
+});
+
+app.use((req, res, next) => {
+    process.stdout.write(req.route.stack[0].method.toUpperCase() + ' ' + req.route.path + '\n');
+    next();
 });
 
 /**
@@ -17,3 +28,16 @@ app.get('/news', (req, res) => {
  */
 app.listen(3000);
 console.log('listening on 3000');
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
